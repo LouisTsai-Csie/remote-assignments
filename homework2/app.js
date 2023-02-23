d5fd093ded78c6bb53c8464dc6ba5c01434e65d0
LouisTsai-Csie <Q1030176@gmail.com>
2023-02-23 21:25:50 +0800

    remote-week2: assignment2

---
+++ homework2/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res)=>{
+    res.send('Hello, world');
+}) 
+
+app.get('/healthcare', (req, res)=>{
+    res.send('OK');
+})
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`);
+});
